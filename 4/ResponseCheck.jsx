5b000e16d2ca2abb67745c00fc7b7838dfa2e2f0
twinkite <yeonni_9657@naver.com>
2021-06-28 23:21:41 +0900

    useRef, setTimeout

--- 4/ResponseCheck.jsx
+++ 4/ResponseCheck.jsx
@@ -56,6 +56,16 @@ const ResponseCheck = () => {
       >
         {message}
       </div>
+      {/* {(() => {
+        if (result.length === 0) {
+          return null;
+        } else {
+          return <>
+            <div>평균 시간 : {result.reduce((a, c) => a + c) / result.length }ms</div>
+            <button onClick={onReset}>리셋</button>
+          </> 
+        }
+      })()} */}
       {renderAverage()}
     </>
   );
