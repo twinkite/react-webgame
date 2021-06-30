d250fa59440addf927acb7f2667e8e52ead6b302
twinkite <yeonni_9657@naver.com>
2021-06-30 14:37:23 +0900

    modify RSP

--- 5/Try.jsx
+++ 5/Try.jsx
@@ -1,8 +1,8 @@
 import React, { memo } from 'react';
 
-const Try = memo(({ TryInfo }) => {
+const Try = memo(({ tryInfo }) => {
   return (
-      <li>{ TryInfo }</li>    
+      <li>{ tryInfo }</li>    
   )
 })
 
